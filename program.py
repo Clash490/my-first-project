f483cc7a5dde0db8443dc8e2a8eabf199d86edd4
Clash490 <klimkabatskov57@gmail.com>
2025-12-26 00:20:58 +0300

    Create program.py
    Генератор случайного числа

---
+++ program.py
@@ -0,0 +1,7 @@
+import random
+
+# Генератор случайного числа
+min_num = 1
+max_num = 100
+random_number = random.randint(min_num, max_num)
+print(f"Случайное число от {min_num} до {max_num}: {random_number}")
